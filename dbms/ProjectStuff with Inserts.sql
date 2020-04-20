f0a9170c9508d24e48da819c433e539c7e4e3b73
HyltonWilliamson <47576081+HyltonWilliamson@users.noreply.github.com>
2020-04-20 18:20:48 -0400

    Updated to more match ERD

--- dbms/ProjectStuff with Inserts.sql
+++ dbms/ProjectStuff with Inserts.sql
@@ -9,33 +9,21 @@ CREATE TABLE customer (
     PRIMARY KEY(customer_id)
     );
     
-    INSERT INTO customer (first_name,last_name,address, city,state, postal_code) VALUES ('John','Smith', '1234 Temp Lane','Orlando','FL',32816);
-    INSERT INTO customer (first_name,last_name,address, city,state, postal_code) VALUES ('Jane','Doe', '5678 Orarily Drive','Orlando','FL',32816);
-    INSERT INTO customer (first_name,last_name,address, city,state, postal_code) VALUES ('Philip','Fry','1729 Future Street', 'New New York','NY',10010);
-    INSERT INTO customer (first_name,last_name,address, city,state, postal_code) VALUES ('Joe','Exotic','25803 N County Road 3250','Wynnewood','OK',73098);
-    INSERT INTO customer (first_name,last_name,address, city,state, postal_code) VALUES ('Kobe','Bryant','5555 Toosoon Court','Somewhere','KB',12345);
-
+CREATE TABLE Supplier (
+CompanyName VARCHAR(40) NOT NULL,
+SupplierID INT NOT NULL,
+PRIMARY KEY(SupplierID)
+);
+    
 CREATE TABLE item (
 	itemID INT NOT NULL AUTO_INCREMENT,
     itemName VARCHAR(100),
     itemPrice DECIMAL(6,2),
-    PRIMARY KEY(itemID)
+    SupplierID INT NOT NULL,
+    PRIMARY KEY(itemID),
+    FOREIGN KEY(SupplierID) REFERENCES Supplier(SupplierID) ON DELETE CASCADE ON UPDATE CASCADE
 	);
     
-    INSERT INTO item (itemName, itemPrice) VALUES ('milk',3.00);
-    INSERT INTO item (itemName, itemPrice) VALUES ('egg',3.38);
-    INSERT INTO item (itemName, itemPrice) VALUES ('power',69.42);
-    INSERT INTO item (itemName, itemPrice) VALUES ('games',59.99);
-    INSERT INTO item (itemName, itemPrice) VALUES ('cups',1.35);
-    INSERT INTO item (itemName, itemPrice) VALUES ('bowl',2.50);
-    INSERT INTO item (itemName, itemPrice) VALUES ('magic',75.00);
-    INSERT INTO item (itemName, itemPrice) VALUES ('soap',35.00);
-    INSERT INTO item (itemName, itemPrice) VALUES ('lotion',3.00);
-    INSERT INTO item (itemName, itemPrice) VALUES ('tissues',2.00);
-    INSERT INTO item (itemName, itemPrice) VALUES ('gamergirl bathwater',99.99);
-    INSERT INTO item (itemName, itemPrice) VALUES ('lunch',5.00);
-    INSERT INTO item (itemName, itemPrice) VALUES ('poison',40.00);
-
 CREATE TABLE discount_codes (
 	code VARCHAR(20),
     item_id INT,
@@ -48,11 +36,9 @@ CREATE TABLE discount_codes (
         ON DELETE CASCADE
 	);
     
-    -- User makes the code, picks item from drop down list, and the customer id(php) and expiration are added automatically(php).
-	INSERT INTO discount_codes (code, item_id, customer_id,expiration) VALUES ('unlimited',3,4,'2020-05-14');
-    INSERT INTO discount_codes (code, item_id, customer_id,expiration) VALUES ('potter',7,3,'2020-05-14');
-    INSERT INTO discount_codes (code, item_id, customer_id,expiration) VALUES ('lul',11,2,'2020-05-14');
-
+CREATE INDEX discount_code_idx
+ON discount_codes (code);
+    
 CREATE TABLE orders (
 	orderID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	customerID INT NOT NULL,
@@ -63,6 +49,45 @@ CREATE TABLE orders (
     FOREIGN KEY (item) REFERENCES item (itemID)  ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (code) REFERENCES discount_codes(code)  ON DELETE CASCADE ON UPDATE CASCADE
 	);
+
+CREATE TABLE BillingInfo (
+	BillingID INT,
+    CreditCardNum VARCHAR(16),
+    EXPDate DATE,
+    CVC INT,
+	address VARCHAR(100),
+    city VARCHAR(40),
+    state CHAR(2),
+    postal_code INT,
+    OrderID INT,
+    PRIMARY KEY (BillingID),
+    FOREIGN KEY(OrderID) REFERENCES orders (orderID) ON DELETE CASCADE
+);
+
+-- User makes the code, picks item from drop down list, and the customer id(php) and expiration are added automatically(php).
+INSERT INTO discount_codes (code, item_id, customer_id,expiration) VALUES ('unlimited',3,4,'2020-05-14');
+INSERT INTO discount_codes (code, item_id, customer_id,expiration) VALUES ('potter',7,3,'2020-05-14');
+INSERT INTO discount_codes (code, item_id, customer_id,expiration) VALUES ('lul',11,2,'2020-05-14');
+
+INSERT INTO customer (first_name,last_name,address, city,state, postal_code) VALUES ('John','Smith', '1234 Temp Lane','Orlando','FL',32816);
+INSERT INTO customer (first_name,last_name,address, city,state, postal_code) VALUES ('Jane','Doe', '5678 Orarily Drive','Orlando','FL',32816);
+INSERT INTO customer (first_name,last_name,address, city,state, postal_code) VALUES ('Philip','Fry','1729 Future Street', 'New New York','NY',10010);
+INSERT INTO customer (first_name,last_name,address, city,state, postal_code) VALUES ('Joe','Exotic','25803 N County Road 3250','Wynnewood','OK',73098);
+INSERT INTO customer (first_name,last_name,address, city,state, postal_code) VALUES ('Kobe','Bryant','5555 Toosoon Court','Somewhere','KB',12345);
+
+INSERT INTO item (itemName, itemPrice) VALUES ('milk',3.00);
+INSERT INTO item (itemName, itemPrice) VALUES ('egg',3.38);
+INSERT INTO item (itemName, itemPrice) VALUES ('power',69.42);
+INSERT INTO item (itemName, itemPrice) VALUES ('games',59.99);
+INSERT INTO item (itemName, itemPrice) VALUES ('cups',1.35);
+INSERT INTO item (itemName, itemPrice) VALUES ('bowl',2.50);
+INSERT INTO item (itemName, itemPrice) VALUES ('magic',75.00);
+INSERT INTO item (itemName, itemPrice) VALUES ('soap',35.00);
+INSERT INTO item (itemName, itemPrice) VALUES ('lotion',3.00);
+INSERT INTO item (itemName, itemPrice) VALUES ('tissues',2.00);
+INSERT INTO item (itemName, itemPrice) VALUES ('gamergirl bathwater',99.99);
+INSERT INTO item (itemName, itemPrice) VALUES ('lunch',5.00);
+INSERT INTO item (itemName, itemPrice) VALUES ('poison',40.00);
 
 INSERT INTO orders (customerID, item, code, quantity) VALUES (1,9,NULL,5);
 INSERT INTO orders (customerID, item, code, quantity) VALUES (1,7,'potter',3);
