83090f020605cb9e6732e0bcc375344272570787
Jacob McGee <akira_kazumi_42@Jacobs-MacBook-Pro.local>
2020-04-22 15:35:09 -0400

    added a bunch of javascript to get the page and database to talk

---
+++ html/scripts/mysql.js
@@ -0,0 +1,26 @@
+function sqlQuery(q) {
+  var mysql = require(['mysql']);
+  var resultSet = [];
+
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "webaccess",
+    password: "cop4710proj",
+    database: "project_schema"
+  });
+
+  con.connect(function(err) {
+    if(err) {
+      console.log("Error! Cannot connect to database. Error log below.");
+      throw err;
+    }
+    console.log("Connected to database.");
+  })
+  con.query(q, function(err, result){
+    if(err){
+      console.log("Error! Unable to process query. Error log below.");
+      throw err;
+    }
+    console.log(result);
+  });
+};
